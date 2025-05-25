c42bee81c6c18b43ac7df61e2e9971df746ec2ac
earth0922 <otegaru0922@gmail.com>
2025-05-25 18:28:05 +0900

    Updates

---
+++ assets/About-BUYemqh_.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-j1nFjs3X.js";import{H as t}from"./Header-BN50jmws.js";function o(){return r.jsxs(r.Fragment,{children:[r.jsx(t,{}),r.jsx("div",{})]})}export{o as default};
+//# sourceMappingURL=About-BUYemqh_.js.map
