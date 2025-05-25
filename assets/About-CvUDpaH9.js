6d05049167a39211a74460904dec32c2becf328e
earth0922 <otegaru0922@gmail.com>
2025-05-25 18:20:27 +0900

    Updates

--- assets/About-CvUDpaH9.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./index-BDP6ko88.js";import{H as t}from"./Header-Cav0NItV.js";function o(){return r.jsxs(r.Fragment,{children:[r.jsx(t,{}),r.jsx("div",{})]})}export{o as default};
-//# sourceMappingURL=About-CvUDpaH9.js.map
