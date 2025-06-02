8201f5f3831eb57cb0d58002ab61f6c89903cc54
earth0922 <otegaru0922@gmail.com>
2025-06-03 08:53:25 +0900

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/home/',  // リポジトリ名を指定
+  base: '/homepage/',  // リポジトリ名を指定
   build: {
     outDir: 'dist',
     sourcemap: true
