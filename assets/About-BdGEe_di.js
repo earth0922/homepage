c789598c65dc1560efd136a0b13b05c65aba68d8
earth0922 <otegaru0922@gmail.com>
2025-06-03 08:43:04 +0900

    Updates

---
+++ assets/About-BdGEe_di.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-BOM0iBa0.js";import{H as t}from"./Header-DGbBYckO.js";function o(){return r.jsxs(r.Fragment,{children:[r.jsx(t,{}),r.jsx("div",{})]})}export{o as default};
+//# sourceMappingURL=About-BdGEe_di.js.map
